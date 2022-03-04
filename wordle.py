16c8d0f524319878062710ea4452c1d98cbe04fe
Giovanni Brusadelli <giobrus99@gmail.com>
2022-03-04 20:40:47 +0100

    load script

---
+++ wordle.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+os.system('cls')
+
+words = open("wordle.txt", "r").read().split(",")
+words = [x[1:-1] for x in words]
+
+y = np.zeros(5, dtype=str)
+for i in range(0, len(y)):
+    y[i] = input("letter in position "+str(i)+"? ")
+
+for i in range(0, len(y)):
+    if y[i] != "":
+        words = [x for x in words if x[i] == y[i]]
+    else:
+        continue
+
+print(words)
