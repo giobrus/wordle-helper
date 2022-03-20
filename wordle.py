859e88a561fee6c3ab83f0baba6622de7a30bdef
Giovanni Brusadelli <giobrus99@gmail.com>
2022-03-20 14:27:07 +0100

    Minor refactor

--- wordle.py
+++ wordle.py
@@ -5,17 +5,17 @@ os.system('cls')
 words = open("wordle.txt", "r").read().split(",")
 words = [x[1:-1] for x in words]
 
-y = np.zeros(5, dtype=str)
-for i in range(0, len(y)):
-    y[i] = input("letter in position "+str(i+1)+"? ")
+green = np.zeros(5, dtype=str)
+for i in range(0, len(green)):
+    green[i] = input("letter in position " + str(i + 1) + "? ")
 
-black = np.zeros(int(input("how many")), dtype=str)
+black = np.zeros(int(input("insert your number of black letters")), dtype=str)
 for i in range(0, len(black)):
     black[i] = input("black letter number "+str(i+1)+": ")
 
-for i in range(0, len(y)):
-    if y[i] != "":
-        words = [x for x in words if x[i] == y[i] and set(x).isdisjoint(black)]
+for i in range(0, len(green)):
+    if green[i] != "":
+        words = [x for x in words if x[i] == green[i] and set(x).isdisjoint(black)]
     else:
         continue
 
