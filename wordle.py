66754cce93ad6c7823141a5836e0eb9bd0f166c7
Giovanni Brusadelli <giobrus99@gmail.com>
2022-03-27 15:36:31 +0200

    improved black letter feature

--- wordle.py
+++ wordle.py
@@ -9,9 +9,16 @@ green = np.zeros(5, dtype=str)
 for i in range(0, len(green)):
     green[i] = input("letter in position " + str(i + 1) + "? ")
 
-black = np.zeros(int(input("insert your number of black letters")), dtype=str)
-for i in range(0, len(black)):
-    black[i] = input("black letter number "+str(i+1)+": ")
+# black = np.zeros(int(input("insert your number of black letters")), dtype=str)
+# for i in range(0, len(black)):
+#     black[i] = input("black letter number "+str(i+1)+": ")
+black = []
+while True:
+    letter = input("input a black letter")  # to exit the loop, press empty enter
+    if letter == "":
+        break
+    else:
+        black.append(letter)
 
 for i in range(0, len(green)):
     if green[i] != "":
